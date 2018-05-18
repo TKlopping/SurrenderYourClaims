86d6244167c9ede085400f18733344e24693eaca
Tessa Schiele <tschiele@retrorabbit.co.za>
2018-05-18 15:48:23 +0200

    More learning/testing and adding of functions and alias's

--- src/BasicsTesting.elm
+++ src/BasicsTesting.elm
@@ -4,20 +4,40 @@ import String
 import Html exposing (..)
 import Html.Events exposing (..)
 
+type alias Model =
+  { heading : String }
+
+type Msg
+    = NoOp
+
+main : Program Never Model Msg
 main =
-  beginnerProgram { model = "", view = view, update = update }
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
--- UPDATE
+init : ( Model, Cmd Msg )
+init = ( { heading = "Surrender your Claims" }, Cmd.none )
 
-type Msg = NewContent String
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
 
-update (NewContent content) olderContent = content
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 -- VIEW
 
+view : Model -> Html Msg
 view content =
     div []
-    [ h1 [ myStyle ] [ text "Surrender your claims"]
+    [ h1 [ myStyle ] [ text content.heading ]
     , img [ src "../assets/marvin.png", imgStyle  ] []
     ]
 
